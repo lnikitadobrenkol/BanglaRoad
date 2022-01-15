8ceb041509bee60efe443307e8dea4803c1ab5cd
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2022-01-15 20:36:01 +0200

    Setup for a PlayerDetails component.

--- bangla_road_app/src/components/Player.js
+++ bangla_road_app/src/components/Player.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import PlayerDetails from "./PlayerDetails";
 
 function Player(props) {
     return (
         <div className="c-player">
             <audio></audio>
             <h4>Playing now</h4>
-            {/* DETAILS */}
+            <PlayerDetails song={props.song} />
+
             {/* CONTROLS */}
             <p><strong>Next up:</strong> {props.nextSong.title} by {props.nextSong.artist}</p>
         </div>
